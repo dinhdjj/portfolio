97f289be6528f6b492e08c618b6bbcc17376210f
dinhdjj <dinhdjj@gmail.com>
2023-03-11 12:43:37 +0700

    chore: fix jsx syntax

--- icons/CircleArrow.tsx
+++ icons/CircleArrow.tsx
@@ -9,9 +9,9 @@ export default function CircleArrow({ className }: { className?: string }) {
       <path
         d='M4.56516 2C-4.43483 11 11.5652 31 32.0651 23.7372C69.0652 -2.5 -20.9348 2 52.0652 40'
         stroke='currentcolor'
-        stroke-width='3'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='3'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M58.8364 40.6527L52.2568 33.0342C51.6512 32.333 50.5 32.7613 50.5 33.6878V39.0576C50.5 39.3394 50.3811 39.608 50.1727 39.7976L47.5132 42.2153C46.7752 42.8862 47.3665 44.1056 48.3503 43.9416L58.244 42.2927C59.0137 42.1644 59.3464 41.2432 58.8364 40.6527Z'
